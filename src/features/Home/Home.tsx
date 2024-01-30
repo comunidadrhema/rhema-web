2fba2d805ebbb54c908871f3c1b8d3e9582eac93
Eliecer Ramirez <66390583+isEliecer@users.noreply.github.com>
2024-01-29 22:11:50 -0600

    adding social media

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { BsFacebook, BsInstagram, BsYoutube } from "react-icons/bs";
+
 import { inter } from "@/config";
 import styles from "@/styles/Home.module.css";
 
@@ -6,6 +9,20 @@ export default function Home() {
     <main className={styles.main}>
     <div className={styles.center}>
       <h2 className={inter.className}>Sitio en Costruccion</h2>
+    </div>
+    <div className="flex justify-center gap-3">
+      <Link href="https://www.instagram.com/comunidadrhema/?hl=en" aria-label="Instagram link" target="_blank" rel="external">
+        <span aria-hidden className="hidden">Instagram</span>
+        <BsInstagram size={40}/>
+      </Link>
+      <Link href="https://www.facebook.com/comunidadrhemacr" aria-label="Facebook link" target="_blank" rel="external">
+        <span aria-hidden className="hidden">Facebook</span>
+        <BsFacebook size={40}/>
+      </Link>
+      <Link href="https://www.youtube.com/@rhemacomunidadcristiana" aria-label="Youtube Link" target="_blank" rel="external">
+        <span aria-hidden className="hidden">Youtube</span>
+        <BsYoutube size={40}/>
+      </Link>
     </div>
   </main>
   );
